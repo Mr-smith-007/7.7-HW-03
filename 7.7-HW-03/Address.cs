7469398768c7418fea5ec3cd2e7e6184f1f85454
Repnikov.A <repnikov.a@florcom.ru>
2023-08-07 11:28:53 +0300

    Добавлен класс Address

---
+++ 7.7-HW-03/Address.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_7_HW_03
+{
+    class Address
+    {
+        string appartment;
+        string floor;
+        string building;
+        string street;
+        string city;
+        string state;
+        string postalCode;
+        string PostalCode
+        {
+            get
+            {
+                return postalCode;
+            }
+            set
+            {
+                if (value.Length == 6 && Int32.TryParse(value, out int res))
+                    postalCode = value;
+                else
+                    Console.WriteLine("Индекс введен некорректно");
+            }
+        }
+        internal Address(string apt = "1", string fl = "1", string bld = "1", string st = "Lenina", string ct = "City", string sta = "Moscow st", string postCode = "123456")
+        {
+            appartment = apt;
+            floor = fl;
+            building = bld;
+            street = st;
+            city = ct;
+            state = sta;
+            postalCode = postCode;
+        }
+        internal string Show() => $"Адрес: {postalCode}, {state}, {city}, дом {building}, этаж {(String.IsNullOrWhiteSpace(floor) ? "нет" : floor)}, квартира {(String.IsNullOrWhiteSpace(appartment) ? "нет" : appartment)}";
+    }
+}

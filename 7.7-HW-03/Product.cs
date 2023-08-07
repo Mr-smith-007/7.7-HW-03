232072993115e00d9b9f00526180bf76871b4ab6
Repnikov.A <repnikov.a@florcom.ru>
2023-08-07 15:34:08 +0300

    Добавлен класс Product

---
+++ 7.7-HW-03/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_7_HW_03
+{
+    internal class Product
+    {
+        string vendorCode;
+        string name;
+        decimal price;
+        public Product(string vc, string name = "product", decimal price = 0)
+        {
+            vendorCode = vc;
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
--- 7.7-HW-03/Program.cs
+++ 7.7-HW-03/Program.cs
@@ -2,11 +2,12 @@
 
 namespace _7_7_HW_03
 {
-class Programm
-{
-    static void Main(string[] args)
+    class Programm
     {
+        static void Main(string[] args)
+        {
+            Client client1 = new Client(age: 25, name: "Иван", lname: "Иванов");
 
+        }
     }
-}
 }

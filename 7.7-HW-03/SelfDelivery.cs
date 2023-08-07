2269386a14a7db2a00e7e53548f8c7e7d4f0771c
Repnikov.A <repnikov.a@florcom.ru>
2023-08-07 14:57:27 +0300

    Добавлен класс SelfDelivery

---
+++ 7.7-HW-03/SelfDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_7_HW_03
+{
+    class SelfDelivery
+    {
+        private string shopAddress = "Shop Address";
+        internal override void Deliver()
+        {
+            Console.WriteLine($"Оформлен самомывоз по адресу {shopAddress}");
+        }
+
+    }
+}

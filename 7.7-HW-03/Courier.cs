3365188c0aeef58570f4fc86f50feb9bcecb4d4c
Repnikov.A <repnikov.a@florcom.ru>
2023-08-07 12:08:36 +0300

    Добавлен класс Courier

---
+++ 7.7-HW-03/Courier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_7_HW_03
+{
+    class Courier: Person
+    {
+        Contacts contacts;
+        Address address;
+        internal Courier (int age, string name, string lname, Contacts cont, Address addr) : base(age, name, lname)
+        {
+            contacts = cont;
+            address = addr;
+        }
+    }
+}
